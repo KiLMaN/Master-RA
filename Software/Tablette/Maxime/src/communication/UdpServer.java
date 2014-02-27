c1ce645d6ad45236d261b4df9f36b1f621507431
Maxime <mleblanc@cergy.itin.fr>
2014-02-27 09:39:56 +0100

    Create Android TCP/UDP Server

---
+++ Software/Tablette/Maxime/src/communication/UdpServer.java
@@ -0,0 +1,15 @@
+package communication;
+
+public class UdpServer implements Runnable {
+	private int _recievePort;
+	
+	public UdpServer(int recievePort){
+		this._recievePort = recievePort;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
